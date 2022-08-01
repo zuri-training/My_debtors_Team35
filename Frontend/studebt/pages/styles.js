5c9382cb2229cd9911b9cca3e2a2e5d95d857945
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-08-01 13:54:45 +0100

    Input fields style guides incomplete

--- Frontend/studebt/pages/styles.js
+++ Frontend/studebt/pages/styles.js
@@ -2,7 +2,10 @@ const styles = () => {
     return (
         <div>
             <h1>Style Guides</h1>
-            
+            <input type="text" placeholder="placeholder" />
+            <input type="text" placeholder="placeholder" className="success" />
+            <input type="text" placeholder="placeholder" className="warning" />
+            <input type="text" placeholder="placeholder" className="error" />
         </div>
     )
 }
