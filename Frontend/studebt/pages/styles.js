ef45aa3ad0b2d2c1f0fe03d400d6a3892866923b
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-08-01 14:36:31 +0100

    Created buttons style guide

--- Frontend/studebt/pages/styles.js
+++ Frontend/studebt/pages/styles.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 const styles = () => {
     return (
         <div>
@@ -5,7 +6,14 @@ const styles = () => {
             <input type="text" placeholder="placeholder" />
             <input type="text" placeholder="placeholder" className="success" />
             <input type="text" placeholder="placeholder" className="warning" />
-            <input type="text" placeholder="placeholder" className="error" />
+            <input type="text" placeholder="placeholder" className="error" /> <br />
+            <h6>Primary Button</h6>
+            <button className="btn btn-sec">Button</button>
+            <h6>Secondary Button</h6>
+            <button className="btn btn-pri">Button</button>
+            <Link href={''}>
+                <a href="" className="btn btn-pri">Home</a>
+            </Link>
         </div>
     )
 }
