815db7dcd37affcd07a1d6bd61f0c536b99187d5
Goketech <modupe775@gmail.com>
2022-08-02 18:50:10 +0100

    JWT Tokens Authentication

--- Backend/studebt/studebt/urls.py
+++ Backend/studebt/studebt/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify', TokenVerifyView.as_view, name='token_verify')
 ]
