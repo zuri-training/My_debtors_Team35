6b9a7eb4f17f44697d239c8b1fa3c93cbf021c6c
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-08-03 16:29:55 +0100

    School dashboard main center bottom in progress

--- Frontend/studebt/components/SchoolMain.js
+++ Frontend/studebt/components/SchoolMain.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const SchoolMain = () => {
@@ -23,8 +24,89 @@ const SchoolMain = () => {
                         </div>
                     </div>
                 </div>
-                <div className="main-center-top">
-                    
+                <div className="main-center-chart">
+                    <div className="main-center-chart-top">
+                        <p className='mt-bold'>New Debtors</p>
+                        <div className="chart-time">
+                            <p className='st-regular secondary-btn'>This Year</p>
+                        </div>
+                    </div>
+                    <div className="main-center-chart-bottom">
+                        <div className="main-center-chart-bottom-rates">
+                            <p className='mt-bold'>+15 Students</p>
+                            <div className="main-center-chart-bottom-rates-direction success">
+                                <p>+10%</p>
+                            </div>
+                        </div>
+                        <div className="main-center-chart-bottom-chart">
+                            <img src='/images/bar.png' />
+                        </div>
+                    </div>
+                </div>
+                <div className="main-center-bottom">
+                    <div className="main-center-bottom-top">
+                        <h6 className=''>Appeals</h6>
+                        <div className="main-center-bottom-top-more">
+                            <Link href=""><a className='secondary-btn'>More</a></Link>
+                        </div>
+                    </div>
+                    <div className="main-center-bottom-filter">
+                        <div className="main-center-bottom-filter-left">
+                            <div className="main-center-bottom-filter-button">
+                                <p className='nt-bold'>
+                                    99
+                                </p>
+                                <p className='nt-regular'>
+                                    <Link href=''>
+                                        <a className='text-underline'>All</a>
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="main-center-bottom-filter-button">
+                                <p className='nt-bold'>
+                                    12
+                                </p>
+                                <p className='nt-regular'>
+                                    <Link href=''>
+                                        <a className=''>New</a>
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="main-center-bottom-filter-button">
+                                <p className='nt-bold'>
+                                    45
+                                </p>
+                                <p className='nt-regular'>
+                                    <Link href=''>
+                                        <a className=''>Pending</a>
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="main-center-bottom-filter-button">
+                                <p className='nt-bold'>
+                                    12
+                                </p>
+                                <p className='nt-regular'>
+                                    <Link href=''>
+                                        <a className=''>Cleared</a>
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="main-center-bottom-filter-button">
+                                <p className='nt-bold'>
+                                    30
+                                </p>
+                                <p className='nt-regular'>
+                                    <Link href=''>
+                                        <a className=''>Denied</a>
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="main-center-bottom-filter-right">
+
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="school-main-left">
