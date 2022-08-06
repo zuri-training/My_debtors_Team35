72e28f512c07caa3460c2c19fa9fb939c35fc537
abdullmustyy <abdulmusty03@outlook.com>
2022-08-06 10:28:00 +0100

    Resolve conflictt

--- Frontend/studebt/pages/_app.js
+++ Frontend/studebt/pages/_app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
@@ -6,15 +5,6 @@ import "primereact/resources/themes/md-light-indigo/theme.css"; //theme css
 import "../styles/globals.css";
 import "../styles/utilities.css";
 import "../styles/DialogDemo.css";
-=======
-import "primereact/resources/primereact.min.css"                //core css
-import "primeicons/primeicons.css"
-import "primeflex/primeflex.css"
-import "primereact/resources/themes/md-light-indigo/theme.css"   
-import '../styles/theme.css'      
-import '../styles/globals.css'
-import '../styles/utilities.css'
->>>>>>> 40891624864b4c9db61b4eae4c59f4e33042d6e6
 
 import ErrorBoundary from "../components/errorboundary";
 
