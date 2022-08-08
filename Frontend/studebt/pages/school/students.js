77a6213a183b854f18ebca6d604c338abae49c3f
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-08-08 09:48:36 +0100

    Impleted school dashboard overview students and appeal pages desktop and mobile design

--- Frontend/studebt/pages/school/students.js
+++ Frontend/studebt/pages/school/students.js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import PageLayout from '../../components/page-layout'
 import Sidebar from '../../components/Sidebar';
 import SchoolMenuStudentsMain from '../../components/SchoolMenuStudentsMain';
-import SchoolMenuStudentsTopBar from '../../components/SchoolMenuStudentsTopBar';
+import StudentsPageTopBar from '../../components/StudentsPageTopBar';
 import { useState } from 'react';
 
 
@@ -14,7 +14,7 @@ export default function Dashboard ( props ) {
                 
             </div>
             <Sidebar toggle={toggle} setToggle={setToggle}/>
-            <SchoolMenuStudentsTopBar setToggle={setToggle}/>
+            <StudentsPageTopBar setToggle={setToggle} />
             <SchoolMenuStudentsMain />
         </div>
     );
