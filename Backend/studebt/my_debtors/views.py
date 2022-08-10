c61e77802ba229e5fac190729682fc97369e166a
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2022-08-10 18:40:28 +0100

    API Views done.

--- Backend/studebt/my_debtors/views.py
+++ Backend/studebt/my_debtors/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from my_debtors.models import Comment, Dispute
+from my_debtors.serializers import (CommentCreateSerializer, CommentSerializer,
+                                    DisputeCreateSerializer, DisputeSerializer)
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+
+class CommentListApi(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentCreateApi(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
+
+
+class DisputeListApi(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Dispute.objects.all()
+    serializer_class = DisputeSerializer
+
+
+class DisputeCreateApi(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Dispute.objects.all()
+    serializer_class = DisputeCreateSerializer
