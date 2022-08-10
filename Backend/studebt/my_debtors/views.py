ef0dd378fcb1e982da289b59b0d10f62adeac6e6
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2022-08-10 18:35:37 +0100

    Dispute and Comment GET API views done.

--- Backend/studebt/my_debtors/views.py
+++ Backend/studebt/my_debtors/views.py
@@ -1,31 +1,50 @@
+from django.core import serializers
 from my_debtors.models import Comment, Dispute
 from my_debtors.serializers import (CommentCreateSerializer, CommentSerializer,
                                     DisputeCreateSerializer, DisputeSerializer)
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Comment, Dispute
+from .serializers import CommentSerializer, DisputeSerializer
 
 # Create your views here.
 
 
-class CommentListApi(ListAPIView):
+class CommentListAPI(GenericAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-
-class CommentCreateApi(CreateAPIView):
+    def get(self, *args, **kwargs):
+        data = Comment.objects.all().values()
+        return Response(data)#, status=status.HTTP_201_CREATED)
+    
+    
+class CommentCreateAPI(GenericAPIView):
     permission_classes = (IsAuthenticated,)
-    queryset = Comment.objects.all()
-    serializer_class = CommentCreateSerializer
+    serializer_class = CommentSerializer
+    # queryset = Comment.objects.all()
+    
+    def post(self, *args, **kwargs):
+        pass
 
 
-class DisputeListApi(ListAPIView):
+class DisputeListAPI(GenericAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Dispute.objects.all()
     serializer_class = DisputeSerializer
 
+    def get(self, *args, **kwargs):
+        data = Dispute.objects.all().values()
+        return Response(data)
+    
 
-class DisputeCreateApi(CreateAPIView):
+class DisputeCreateAPI(GenericAPIView):
     permission_classes = (IsAuthenticated,)
-    queryset = Dispute.objects.all()
-    serializer_class = DisputeCreateSerializer
+    serializer_class = DisputeSerializer
+    # queryset = Dispute.objects.all()
+    
+    def post(self, request, **kwargs):
+        pass
