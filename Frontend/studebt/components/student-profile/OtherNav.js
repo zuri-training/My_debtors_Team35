a575bd0394835f216b8ee65fd9f6aa9567b5e7c2
abdullmustyy <abdulmusty03@outlook.com>
2022-08-10 02:50:00 +0100

    Add menu dropdown button

--- Frontend/studebt/components/student-profile/OtherNav.js
+++ Frontend/studebt/components/student-profile/OtherNav.js
@@ -1,19 +1,45 @@
 import { Menubar } from "primereact/menubar";
 import Image from "next/image";
 import profilePics from "../../public/assets/images/profile-pics.png";
-import menuIcon from "../../public/assets/images/Menu-icon.svg";
+import React, { useState, useRef } from "react";
+import { Button } from "primereact/button";
+import { Menu } from "primereact/menu";
 import bell from "../../public/assets/images/notification.png";
-import plus from "../../public/assets/images/plus.png";
 
-export default function OtherNav(props) {
-  const menu = (
-    <div className="flex align-items-center gap-2 p-2">
-      <Image src={menuIcon} alt="Menu Icon" />
-      <span className="nav-menu sm:text-2xl text-lg md:inline-block hidden">
-        Menu
-      </span>
+const MenuDropdown = () => {
+  const menu = useRef(null);
+  const items = [
+    {
+      items: [
+        {
+          label: "Home",
+          icon: "pi pi-home",
+        },
+        {
+          label: "About Us",
+          icon: "pi pi-hashtag",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <Menu model={items} popup ref={menu} id="popup_menu" />
+      <Button
+        label="Menu"
+        icon="pi pi-bars"
+        className="p-button-text p-button-lg"
+        onClick={(event) => menu.current.toggle(event)}
+        aria-controls="popup_menu"
+        aria-haspopup
+      />
     </div>
   );
+};
+
+export default function OtherNav(props) {
+  const menu = <MenuDropdown />;
 
   const menuNav = (
     <div className="flex justify-content-end align-items-center md:gap-4 gap-2">
