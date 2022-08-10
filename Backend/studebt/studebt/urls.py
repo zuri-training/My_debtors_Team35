1b93591c87745c30a1f2184861de42c0486fdb43
ebereanuebunwa <70179219+ebereanuebunwa@users.noreply.github.com>
2022-08-10 12:42:27 +0100

    Registered The TokenVerifyView.as_view()

--- Backend/studebt/studebt/urls.py
+++ Backend/studebt/studebt/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify', TokenVerifyView.as_view, name='token_verify'),
+    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
 
     path('api/auth/users/', include('accounts.urls')),
 ]
